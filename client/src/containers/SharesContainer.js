86396db847be3513ee3e31b4ea6b5444df142ded
y <codecharliecode@gmail.com>
2021-08-05 17:26:19 +0100

    Updated shares container

--- client/src/containers/SharesContainer.js
+++ client/src/containers/SharesContainer.js
@@ -1,16 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import TotalsPanel from '../components/TotalsPanel/TotalPanel';
 import SharesTable from '../components/SharesTable/SharesTable';
 import SharesCarousel from '../components/SharesCarousel/SharesCarousel';
 // import ToggleAddForm from '../components/Edit/ToggleAddForm';
 import AddForm from '../components/Edit/AddForm';
+import {getShares} from '../components/ShareService';
 // import ToggleDeleteForm from '../components/Edit/ToggleDeleteForm';
 // import DeleteForm from '../components/Edit/DeleteForm';
 
 
 const SharesContainer = () => {
     
+    const [allShares, setAllShares] = useState({});
+    const [userShares, setUserShares] = useState({});
 
+    useEffect(() => {
+        getShares().then((dbShares) => {
+            setUserShares(dbShares);
+        });
+    })
+    
+    
     const Search = (search_term) => {
         useEffect(() => {
             fetch(`https://ticker-2e1ica8b9.now.sh/keyword/${search_term}`)
