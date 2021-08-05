ce0b6d1ea0e0da41e6102452ac1fcc537bb84f08
Link <xlinkx_@outlook.com>
2021-08-05 10:56:39 +0100

    implement react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
+import HomeContainer from "./containers/HomeContainer";
+import SharesContainer from "./containers/SharesContainer";
+import NavBar from "./components/NavBar/NavBar";
+
+const App = () => {
   return (
-    <></>
+    <Router>
+      <>
+        <NavBar />
+          <Switch>
+            <Route exact path="/" component={HomeContainer}/>
+            <Route path="/portfolio" component={SharesContainer}/>
+          </Switch>
+      </>
+    </Router>
   );
 }
 
