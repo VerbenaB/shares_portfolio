e78bb53a004fe46c90b69c73571c77b74a88071f
Link <xlinkx_@outlook.com>
2021-08-05 11:13:06 +0100

    adds import statements

---
+++ client/src/containers/SharesContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TotalsPanel from './components/TotalsPanel/TotalsPanel';
+import SharesTable from './components/SharesTable/SharesTable';
+import SharesCarousel from './components/SharesCarousel/SharesCarousel';
+import ToggleAddForm from './components/Edit/ToggleAddForm';
+import AddForm from './components/Edit/AddForm';
+import ToggleDeleteForm from './components/Edit/ToggleDeleteForm';
+import DeleteForm from './components/Edit/DeleteForm';
+
+
+import TotalTrends from './components/TotalsPanel/TotalTrends';
+import TotalValue from './components/TotalsPanel/TotalValue';
+
+import ShareRow from './components/SharesTable/SharesRow';
+
+import SharesList from './components/SharesCarousel/SharesList';
+import ShareCard from './components/SharesCarousel/ShareCard';
+
