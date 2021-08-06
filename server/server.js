c6e5be07c704afaa0958020610d9478548f614e3
Verbena <veve292@live.it>
2021-08-06 13:21:03 +0100

    fixed back end api query search

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
+const MongoClient = require("mongodb").MongoClient;
+const createRouter = require("./helpers/create_router.js");
+const createTickerRouter = require("./helpers/create_ticker_router.js");
 
 app.use(express.json());
 
-MongoClient.connect('mongodb://localhost:27017')
+MongoClient.connect("mongodb://localhost:27017")
   .then((client) => {
-    
-    const db = client.db('shares');
-    const sharesCollection = db.collection('portfolio');
+    const db = client.db("shares");
+    const sharesCollection = db.collection("portfolio");
     const sharesRouter = createRouter(sharesCollection);
-    const tickersCollection = db.collection('tickers');
-    const tickersRouter = createRouter(tickersCollection);
-    app.use('/api/portfolio', sharesRouter);
-    app.use('/api/tickers', tickersRouter);
+    const tickersCollection = db.collection("tickers");
+    const tickersRouter = createTickerRouter(tickersCollection);
+    app.use("/api/portfolio", sharesRouter);
+    app.use("/api/tickers", tickersRouter);
   })
   .catch(console.err);
 
 app.listen(5000, function () {
-  console.log(`Listening on port ${ this.address().port }`);
+  console.log(`Listening on port ${this.address().port}`);
 });
