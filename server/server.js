61e578a431536bb6c450c0cdc335309041bc6974
y <codecharliecode@gmail.com>
2021-08-06 09:45:51 +0100

    Got CSV data imported to Mongo DB though every time the file runs it imports the data again

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const csvtojson = require("csvtojson");
+const csvtojson = require("csvtojson");
 
 const cors = require('cors');
 app.use(cors());
@@ -10,21 +10,32 @@ const createRouter = require('./helpers/create_router.js');
 
 app.use(express.json());
 
-// csvtojson()
-//   .fromFile("listing_status.csv")
-//   .then(csvData => {
-//     console.log(csvData);
+csvtojson()
+  .fromFile("./listing_status.csv")
+  .then(csvData => {
+    console.log(csvData)
+  
+    MongoClient.connect('mongodb://localhost:27017')
+      .then((client) => {
+        
+        const db = client.db('shares');
+        const sharesCollection = db.collection('portfolio');
+        const sharesRouter = createRouter(sharesCollection);
 
-MongoClient.connect('mongodb://localhost:27017')
-  .then((client) => {
-    
-    const db = client.db('shares');
-    const sharesCollection = db.collection('portfolio');
-    const sharesRouter = createRouter(sharesCollection);
-    app.use('/api/portfolio', sharesRouter);
-  })
-  .catch(console.err);
+        const tickersCollection = db.collection('tickers');
+        tickersCollection.insertMany(csvData, (err, res) => {
+          if (err) throw err;
+          console.log(`Inserted: ${res.insertedCount} rows`);
+        })
+      
+        app.use('/api/portfolio', sharesRouter);
+        // client.close();
+      })
+      .catch(console.err)
 
-app.listen(5000, function () {
-  console.log(`Listening on port ${ this.address().port }`);
-});
+})
+
+  app.listen(5000, function () {
+    console.log(`Listening on port ${ this.address().port }`);
+  });
+
