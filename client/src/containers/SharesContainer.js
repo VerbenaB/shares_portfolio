dd8187f76491b5c8ebe39dcbce9946894828593b
Verbena <veve292@live.it>
2021-08-09 11:53:31 +0100

    getting info from AlphaVantage'

--- client/src/containers/SharesContainer.js
+++ client/src/containers/SharesContainer.js
@@ -9,23 +9,43 @@ import { getShares, getTickers, postShare } from "../components/ShareService";
 // import DeleteForm from '../components/Edit/DeleteForm';
 
 const SharesContainer = () => {
-  const [selectedShare, setSelectedShare] = useState({});
+  // const [selectedShare, setSelectedShare] = useState({});
   const [portfolioShares, setPortfolioShares] = useState({});
+  const [sharesInfo, setSharesInfo] = useState({});
 
   useEffect(() => {
     getShares().then((dbShares) => {
       setPortfolioShares(dbShares);
+      getInfoFromAV(portfolioShares);
     });
   }, []);
 
-  const searchTicker = (search_term, cb) => {
-    getTickers(search_term).then((res) => cb(res));
+  const getInfoFromAV = (shares) => {
+    if (shares.length > 1) {
+      const fetches = shares.map((share) => {
+        return fetch(
+          `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${share.share.symbol}&apikey=${process.env.ALPHAVANTAGE_KEY}`
+        ).then((res) => res.json());
+      });
+      Promise.all(fetches)
+        .then((results) => {
+          setSharesInfo(results)
+        })
+    } else {
+      return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${portfolioShares[0].share.symbol}&apikey=${process.env.ALPHAVANTAGE_KEY})
+    }
+    
     
   };
 
+  const searchTicker = (search_term, cb) => {
+    getTickers(search_term).then((res) => cb(res));
+  };
+
   const shareSubmit = (shareObject) => {
-    postShare(shareObject)
-      .then(setPortfolioShares([...portfolioShares, shareObject]));
+    postShare(shareObject).then(
+      setPortfolioShares([...portfolioShares, shareObject])
+    );
   };
 
   return (
