9cc02b128ae78a78911a25758be02417ce96b575
Verbena <veve292@live.it>
2021-08-09 12:37:30 +0100

    added Highcharts to the nodemodules

--- client/src/containers/SharesContainer.js
+++ client/src/containers/SharesContainer.js
@@ -5,6 +5,7 @@ import SharesCarousel from "../components/SharesCarousel/SharesCarousel";
 // import ToggleAddForm from '../components/Edit/ToggleAddForm';
 import AddForm from "../components/Edit/AddForm";
 import { getShares, getTickers, postShare } from "../components/ShareService";
+import Highcharts from ""
 // import ToggleDeleteForm from '../components/Edit/ToggleDeleteForm';
 // import DeleteForm from '../components/Edit/DeleteForm';
 
@@ -19,10 +20,6 @@ const SharesContainer = () => {
     })
     
       // getInfoFromAV(portfolioShares)
-    
-    
-    
-    
     
 
   }, []);
