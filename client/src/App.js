6d1861d005a5167f7ca931ad17efe2d39f9117fc
Link <xlinkx_@outlook.com>
2021-08-10 14:12:35 +0100

    adds navbar and homecontainer

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
-// import HomeContainer from "./containers/HomeContainer";
+import HomeContainer from "./containers/HomeContainer";
 import SharesContainer from "./containers/SharesContainer";
-// import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <Router>
       <>
-        {/* <NavBar /> */}
+        <NavBar />
           <Switch>
-            {/* <Route exact path="/" component={HomeContainer}/> */}
+            <Route exact path="/" component={HomeContainer}/>
             <Route path="/portfolio" component={SharesContainer}/>
           </Switch>
       </>
---
+++ client/src/containers/HomeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeContainer = () => {
+    return (
+    <>
+        <p>Hello, this is home</p>
+    </>
+    )
+}
+
+export default HomeContainer;
