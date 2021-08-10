e0957d87136ed762d6bdd001437071550ee84868
Link <xlinkx_@outlook.com>
2021-08-10 14:54:23 +0100

    navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,16 +1,29 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {AppBar, Toolbar, Typography, useMediaQuery, Menu, MenuItem, Button} from "@material-ui/core";
+
 
 const NavBar = () => {
+    
+    // const [anchor, setAnchor] = React.useState(null);
+    // const open = Boolean(anchor);
+
+    // const handleMenu = (event) => {
+    //     setAnchor(event.currentTarget);
+    //  };
+
+    // const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+    
     return (
-        <ul>
-            <li className="link">
-            <Link to="/">Home</Link>
-            </li>
-            <li className="link">
-            <Link to="/portfolio">Portfolio</Link>
-            </li>
-        </ul>
+       
+        <AppBar position="static" color="inherit">
+            <Toolbar variant="dense">
+                <Typography variant="h6">Shares Portfolio</Typography>
+                <Link to="/" style={{ textDecoration: 'none' }}><MenuItem>Home</MenuItem></Link>
+                <Link to="/portfolio" style={{ textDecoration: 'none' }}><MenuItem>Portfolio</MenuItem></Link>
+            </Toolbar>
+        </AppBar>
+
     )
 }
 
