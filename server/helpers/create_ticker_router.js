37105208d1ee4f7e17623c4c6cb8545f7137514e
Verbena <veve292@live.it>
2021-08-11 15:02:11 +0100

    delete db and seeds file

--- server/helpers/create_ticker_router.js
+++ server/helpers/create_ticker_router.js
@@ -4,18 +4,6 @@ const ObjectID = require("mongodb").ObjectID;
 const createTickerRouter = function (collection) {
   const router = express.Router();
 
-  //   router.get("/", (req, res) => {
-  //     collection
-  //       .find()
-  //       .toArray()
-  //       .then((docs) => res.json(docs))
-  //       .catch((err) => {
-  //         console.error(err);
-  //         res.status(500);
-  //         res.json({ status: 500, error: err });
-  //       });
-  //   });
-
   router.get("/", (req, res) => {
     const cursor = collection.find({ name: { $in: [req.query.search_term] } });
     cursor
